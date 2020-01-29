52e2ea31f6b12b6033c0287ce36cb069d9ad561b
Home Prontsevich <prontsevich@gmail.com>
2020-01-30 00:20:38 +0300

    feat(player-movement): created Player Input Actions; created player movement controller

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerController : MonoBehaviour
+{
+    public Camera camera;
+
+    public float moveSpeed;
+    public float turnSpeed;
+
+    public float lerpSpeed = 3f;
+
+    private Quaternion newCameraRotation;
+    private Quaternion newForwardRotation;
+
+    private CharacterController body;
+
+    private float forward;
+    private float horizontal;
+    private float look;
+    private float turn;
+    private float yRotation;
+    private float xRotation;
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        Vector2 contextV2 = context.ReadValue<Vector2>();
+        Debug.Log("Move");
+        Debug.Log(contextV2);
+        horizontal = contextV2.x;
+        forward = contextV2.y;
+    }
+
+    public void Look(InputAction.CallbackContext context)
+    {
+        //Debug.Log("Look");
+        Vector2 contextV2 = context.ReadValue<Vector2>();
+        turn = contextV2.x;
+        look = contextV2.y;
+    }
+
+    private void Awake()
+    {
+        //Cursor.lockState = CursorLockMode.Locked;
+
+        body = GetComponent<CharacterController>();
+
+        newForwardRotation = transform.rotation;
+        newCameraRotation = camera.transform.localRotation;
+
+    }
+
+    private void FixedUpdate()
+    {
+        //body.velocity = Vector3.zero;
+
+        //Turn the player based on the Turn Axis
+        yRotation = transform.rotation.eulerAngles.y;
+        yRotation += turnSpeed * turn;
+        newForwardRotation = Quaternion.Euler(transform.rotation.eulerAngles.x, yRotation, transform.rotation.eulerAngles.z);
+
+        //Pitch the camera based on the Look Axis
+        xRotation = camera.transform.localRotation.eulerAngles.x;
+        xRotation -= turnSpeed * look;
+        newCameraRotation = Quaternion.Euler(xRotation, camera.transform.localRotation.eulerAngles.y, camera.transform.localRotation.eulerAngles.z);
+
+        Vector3 direction = new Vector3(horizontal, 0, forward);
+        body.Move(direction * moveSpeed * Time.deltaTime);
+        //Move the rigidbody based on forward input
+        //body.AddForce(transform.forward * 1000 * moveSpeed * forward);
+
+        //Move the rigidbody based on horizontal input
+        //body.AddForce(transform.right * 1000 * moveSpeed * horizontal);
+
+        //Lerp the rotations
+        transform.rotation = Quaternion.Lerp(transform.rotation, newForwardRotation, lerpSpeed);
+        //camera.transform.localRotation = Quaternion.Lerp(camera.transform.localRotation, newCameraRotation, lerpSpeed);
+
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovementController : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed = 5f;
+    [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private float mass = 1f;
+    [SerializeField] private float damping = 5f;
+
+    private CharacterController characterController;
+    private float velocityY;
+    private Vector3 currentImpact;
+    private Vector3 movementInput;
+    private bool isJump;
+
+    private readonly float gravity = Physics.gravity.y;
+
+    public void MoveInput(InputAction.CallbackContext context)
+    {
+        Vector2 contextV2 = context.ReadValue<Vector2>();
+        movementInput = new Vector3(contextV2.x, 0f, contextV2.y);
+    }
+
+    public void JumpInput(InputAction.CallbackContext context)
+    {
+        float contextJump = context.ReadValue<float>();
+        isJump = contextJump > 0;
+    }
+
+    public void AddForce(Vector3 direction, float magnitude)
+    {
+        currentImpact += direction.normalized * magnitude / mass;
+    }
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    private void Move()
+    {
+        if (characterController.isGrounded && velocityY < 0f)
+        {
+            velocityY = 0f;
+        }
+
+        velocityY += gravity * Time.deltaTime;
+
+        Vector3 velocity = movementInput * movementSpeed + Vector3.up * velocityY;
+
+        if (currentImpact.magnitude > 0.2f)
+        {
+            velocity += currentImpact;
+        }
+
+        characterController.Move(velocity * Time.deltaTime);
+
+        currentImpact = Vector3.Lerp(currentImpact, Vector3.zero, damping * Time.deltaTime);
+    }
+
+    private void Jump()
+    {
+        if (isJump && characterController.isGrounded)
+        {
+            AddForce(Vector3.up, jumpForce);
+        }
+    }
+}
