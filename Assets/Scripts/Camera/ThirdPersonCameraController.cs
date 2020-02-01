6d11400ddaa3f0ce2fe6d15cdea6bd6a4abaf8e8
Sergey <prontsevich@gmail.com>
2020-02-01 14:12:50 +0300

    feat(camera): can move camera around the player

---
+++ Assets/Scripts/Camera/ThirdPersonCameraController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ThirdPersonCameraController : MonoBehaviour
+{
+    [SerializeField] private Transform lookAt;
+    [SerializeField] private float distance = 6f;
+    [SerializeField] private float currentX = 0f;
+    [SerializeField] private float currentY = 20f;
+    [SerializeField] private float sensitivityX = 4f;
+    [SerializeField] private float sensitivityY = 1f;
+    [SerializeField] private bool inverseY = false;
+    [SerializeField] private float rotationSmoothTime = 1.2f;
+
+    private const float Y_ANGLE_MIN = 5f;
+    private const float Y_ANGLE_MAX = 50f;
+
+    private Transform cameraTransform;
+    private Camera cam;
+    private Vector2 lookInput;
+    private Vector3 rotationSmoothVelocity;
+    private Vector3 currentRotation;
+
+    public void LookInput(InputAction.CallbackContext context)
+    {
+        Vector2 contextV2 = context.ReadValue<Vector2>();
+        lookInput = contextV2;
+    }
+
+    private void Start()
+    {
+        cameraTransform = transform;
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        currentX += lookInput.x * sensitivityX;
+        currentY = currentY + (inverseY ? -1 : 1) * (lookInput.y * sensitivityY);
+
+        currentY = Mathf.Clamp(currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
+    }
+
+    private void LateUpdate()
+    {
+        currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(currentY, currentX),
+            ref rotationSmoothVelocity, rotationSmoothTime);
+        cameraTransform.eulerAngles = currentRotation;
+        cameraTransform.position = lookAt.position - cameraTransform.forward * distance;
+
+        //Vector3 dir = new Vector3(0, 0, -distance);
+        //Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+        //cameraTransform.position = lookAt.position + rotation * dir;
+        //cameraTransform.LookAt(lookAt.position);
+    }
+}
--- Assets/Scripts/Player/PlayerMovementController.cs
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -5,22 +5,31 @@ using UnityEngine.InputSystem;
 public class PlayerMovementController : MonoBehaviour
 {
     [SerializeField] private float movementSpeed = 5f;
+    [SerializeField] private float rotationSpeed = 3f;
     [SerializeField] private float jumpForce = 5f;
     [SerializeField] private float mass = 1f;
     [SerializeField] private float damping = 5f;
+
+    private Transform cameraTransform;
 
     private CharacterController characterController;
     private float velocityY;
     private Vector3 currentImpact;
     private Vector3 movementInput;
+    private Vector2 inputDir;
     private bool isJump;
+    private Quaternion targetRot;
+
+    private float turnSmoothVelocity;
+    private float turnSmoothTime = 0.1f;
 
     private readonly float gravity = Physics.gravity.y;
 
     public void MoveInput(InputAction.CallbackContext context)
     {
         Vector2 contextV2 = context.ReadValue<Vector2>();
-        movementInput = new Vector3(contextV2.x, 0f, contextV2.y);
+        inputDir = contextV2.normalized;
+        movementInput = new Vector3(0, 0f, contextV2.y);
     }
 
     public void JumpInput(InputAction.CallbackContext context)
@@ -37,6 +46,8 @@ public class PlayerMovementController : MonoBehaviour
     private void Awake()
     {
         characterController = GetComponent<CharacterController>();
+        cameraTransform = Camera.main.transform;
+        targetRot = transform.rotation;
     }
 
     // Update is called once per frame
@@ -55,14 +66,23 @@ public class PlayerMovementController : MonoBehaviour
 
         velocityY += gravity * Time.deltaTime;
 
-        Vector3 velocity = movementInput * movementSpeed + Vector3.up * velocityY;
+
+        if (inputDir != Vector2.zero)
+        {
+            float targetRotation = Mathf.Atan2(inputDir.x, 0) * Mathf.Rad2Deg + cameraTransform.eulerAngles.y;
+            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSmoothTime);
+        }
+
+        //Vector3 velocity = movementInput.normalized * movementSpeed + Vector3.up * velocityY;
+        Vector3 velocity = movementInput.normalized * movementSpeed + Vector3.up * velocityY + transform.up * rotationSpeed * inputDir.x * Time.deltaTime;
 
         if (currentImpact.magnitude > 0.2f)
         {
             velocity += currentImpact;
         }
 
-        characterController.Move(velocity * Time.deltaTime);
+        //characterController.Move(velocity * Time.deltaTime);
+        characterController.SimpleMove(transform.forward * movementSpeed * movementInput.normalized.z);
 
         currentImpact = Vector3.Lerp(currentImpact, Vector3.zero, damping * Time.deltaTime);
     }
